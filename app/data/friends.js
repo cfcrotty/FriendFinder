01a107786267918ea67f30914684666840ea7294
cfcrotty <carafeliselana@gmail.com>
2019-03-31 12:38:38 -0700

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ const tableData = [
       name: "Three",
       photo: "https://cdn.pixabay.com/photo/2013/07/18/10/56/smiley-163510_1280.jpg",
       scores: [
-          3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3,
-          3
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3",
+          "3"
       ]
     },
     {
